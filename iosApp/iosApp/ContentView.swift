087d62ca2fbe174ba9308faf81fa9a5abf49eec7
Winston Sung <winston21.sung@gmail.com>
2025-10-13 13:56:15 +0800

    Localizable

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,12 +1,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    var world: String = "world"
+
+    var ios: String = "iOS"
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Text(
+                String(
+                    localized: "content_greeting",
+                    defaultValue: "Hello, \(world), \(ios)!",
+                    comment: "Greeting text in content"
+                )
+            )
+
+            Text(
+                AttributedString(
+                    localized: "content_greeting_styled",
+                    defaultValue: "**Hello**, \(world), \(ios)!",
+                    comment: "Styled greeting text in content"
+                )
+            )
         }
         .padding()
     }
